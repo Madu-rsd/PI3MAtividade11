2e9e7528afe73f3cece33b4670d5f6379122f5de
Maria Eduarda <e.rosendo@escolar.ifrn.edu.br>
2026-05-27 16:45:15 -0300

    Atualização final

---
+++ q1.py
@@ -0,0 +1,8 @@
+x = float(input("Digite o primeiro número: "))
+y = float(input("Digite o segundo número: "))
+
+def multiplicacao(x, y):
+    return x * y
+
+resultado = multiplicacao(x, y)
+print(x, "x", y, "=", resultado)
